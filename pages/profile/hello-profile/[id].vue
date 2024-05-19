175583a15bb2d55ec90a1485f635a7e8600dd1eb
Rahatul99 <rahatul99@gmail.com>
2024-05-19 16:07:44 +0600

    first commit

---
+++ pages/profile/hello-profile/[id].vue
@@ -0,0 +1,9 @@
+<template>
+  <div>event id: {{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/profile/hello-profile/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Hello profile</div>
+</template>
+
+<script>
+const nuxtApp = useNuxtApp();
+console.log(nuxtApp, "nuxtApp");
+</script>
+
+<style lang="scss" scoped></style>
